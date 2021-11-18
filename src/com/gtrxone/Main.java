494f4f302ce8cda4ed194daf0a2d55f9cb76e3ee
Goonawardana R N W <gtrxone@gmail.com>
2021-11-19 00:39:38 +0530

    Initial commit

---
+++ src/com/gtrxone/Main.java
@@ -0,0 +1,22 @@
+package com.gtrxone;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(28));
+    }
+    public static boolean isPerfectNumber(int number){
+        if (number >=1){
+            int sum =0;
+            for (int i = 1; i <= number ; i++) {
+                if (number%i == 0){
+                    sum +=i;
+                    if (sum == number){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
